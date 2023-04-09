f94faa7aeaf82111386fbd7e5d2171c4359dbca4
Ahmad Rifaldiansyah <2110031802058@sar.ac.id>
2023-04-09 03:51:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.recyclerviewkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val superheroList = listOf<Superhero>(
+            Superhero(
+                R.drawable.jett,
+                nameSuperhero = "jett" ,
+                descSuperhero = "HP : 110"
+            ),
+            Superhero(
+                R.drawable.yoru,
+                nameSuperhero = "yoru" ,
+                descSuperhero = "HP : 104"
+            ),
+            Superhero(
+                R.drawable.heal,
+                nameSuperhero = "heal" ,
+                descSuperhero = "HP : 110+15"
+            ),
+            Superhero(
+                R.drawable.sage,
+                nameSuperhero = "sage" ,
+                descSuperhero = "HP : 100"
+            ),
+            Superhero(
+                R.drawable.cype,
+                nameSuperhero = "cypher" ,
+                descSuperhero = "HP : 120"
+            ),
+            Superhero(
+                R.drawable.kayo,
+                nameSuperhero = "kay/o" ,
+                descSuperhero = "HP : 110+90"
+            ),
+            Superhero(
+                R.drawable.kiljoy,
+                nameSuperhero = "killjoy" ,
+                descSuperhero = "HP : 120"
+            ),
+            Superhero(
+                R.drawable.jok,
+                nameSuperhero = "jok" ,
+                descSuperhero = "HP : 160"
+            ),
+            Superhero(
+                R.drawable.nora,
+                nameSuperhero = "nora" ,
+                descSuperhero = "HP : 150"
+            ),
+        )
+
+            val recyclerView = findViewById<RecyclerView>(R.id.rv_hero)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = SuperheroAdapter(this, superheroList){
+
+            }
+    }
+}
